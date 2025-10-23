08cafde3ff0b8717ea2c20e2a473fcf1ff08f12a
JasielD <dsilvajasiel@gmail.com>
2025-10-23 23:39:52 +0530

    added next-Auth(github) and theme

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <section className="pink_container bg-primary">
+      <h1 className="heading">Pitch Your Start-up <br/></h1> 
+
+      </section>
+    </div>
+  );
+}
