bd44cf0a1151d7c4afb033bd56f58c8a59a1adbd
JasielD <dsilvajasiel@gmail.com>
2025-10-24 09:17:42 +0530

    heros section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div>
       <section className="pink_container bg-primary">
-      <h1 className="heading">Pitch Your Start-up <br/></h1> 
+      <h1 className="heading">Pitch Your Start-up <br/> Connect with Enterpreneurs</h1> 
+      <p className="sub-heading max-w-3xl">Submit Ideas, Vote on pitches, Get Noticed in Virtual Compitition</p>
 
       </section>
     </div>
