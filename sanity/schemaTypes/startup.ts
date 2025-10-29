587631418d1d41b300918605c6a31f9f3d1ca176
JasielD <dsilvajasiel@gmail.com>
2025-10-29 11:11:58 +0530

    sanity Schema and studio

---
+++ sanity/schemaTypes/startup.ts
@@ -0,0 +1,50 @@
+import { UserIcon } from "lucide-react";
+import { defineField, defineType } from "sanity";
+
+
+export const  startup = defineType({
+    name:'startup',
+    title:'Startup',
+    type: "document",
+    icon:UserIcon,
+    fields:[
+        defineField({
+            name:"name",
+            type:"string"
+        }),
+        defineField({
+            name:"slug",
+            type:"slug",
+            options:{
+                source:"title"
+            }
+        }),
+        defineField({
+            name:"author",
+            type:"reference",
+            to:{type:"author"}
+        }),
+        defineField({
+            name:"views",
+            type:"number"
+        }),
+        defineField({
+            name:"description",
+            type:"text"
+        }),
+        defineField({
+            name:"category",
+            type:"string",
+            validation:(rule)=>rule.min(1).max(20).required().error("please select a category")
+        }),
+           defineField({
+            name:"images",
+            type:"url",
+            validation:(rule)=>rule.required()
+        }),
+           defineField({
+            name:"pitch",
+            type:"markdown"
+        }),
+    ],
+}) 
