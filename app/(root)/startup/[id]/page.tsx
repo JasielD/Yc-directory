f2d205ee852d57a69551c0de8af3939dd5f9232f
JasielD <dsilvajasiel@gmail.com>
2025-11-01 08:50:35 +0530

    understanding parcial pre-rendering

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,10 +1,19 @@
 // [id] was created for dynamic routes eg: url/startup/[id]
+import { client } from '@/sanity/lib/client';
+import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
+export const experimental_ppr = true;
+
+const page = async ({params}:{params:Promise<{id:string}>}) => {
+  const id = (await params).id
+  console.log(id)
+  const posts = await client.fetch(STARTUP_BY_ID_QUERY,{id});
+  if(!posts) return notFound();
   return (
     <div>
-      Page
+      <p className='text-3xl'>{posts.title}</p>
     </div>
   )
 }
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -12,3 +12,18 @@ export const STARTUP_QUERY = defineQuery(`*[_type=="startup" && defined(slug.cur
     image,
     description
 }`)
+
+export const  STARTUP_BY_ID_QUERY = defineQuery(`*[_type=="startup" && _id == $id] [0]{
+  _id,
+    title,
+    category,
+    _createdAt,
+    views,
+    author ->{
+      _id,name,username,image,bio
+    },
+    image,
+    description,
+    pitch,
+    slug
+}`)
