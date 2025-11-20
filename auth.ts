6e9b64c942eee38d76d4340b559168200739132b
JasielD <dsilvajasiel@gmail.com>
2025-11-20 10:33:32 +0530

    nextauth type error

--- auth.ts
+++ auth.ts
@@ -1,41 +1,51 @@
-import NextAuth from "next-auth"
+import  NextAuth  from "next-auth"
 import GitHub from "next-auth/providers/github"
 import { client } from "./sanity/lib/client"
 import { AUTHOR_BY_GITHUB_ID } from "./sanity/lib/queries"
 import { write_Client } from "./sanity/lib/write-client"
- 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+
+export const { handlers, auth, signIn, signOut } = (NextAuth as any)({
   providers: [GitHub],
-  callbacks:{
-    async signIn ({user:{name,email,image},profile:{id,login,bio},account}){
-      const existingUser = await client.withConfig({useCdn:false}).fetch(AUTHOR_BY_GITHUB_ID,{id:id})
-      if(!existingUser){
+  callbacks: {
+    async signIn({ user: { name, email, image }, profile, account }) {
+      // 1. Check if user exists
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(AUTHOR_BY_GITHUB_ID, { id: profile?.id });
+
+      // 2. Create user if they don't exist
+      if (!existingUser) {
         await write_Client.create({
-          _type: 'author',
-          id:id,
-          name:name,
-          username:login,
-          email:email,
-          image:image,
-          bio:bio || "",
+          _type: "author",
+          id: profile?.id,
+          name: name,
+          username: profile?.login,
+          email: email,
+          image: image,
+          bio: profile?.bio || "",
+        });
+      }
 
-        })
-      }
-      if(existingUser){
-        return true;
-      }
+      // 3. ALWAYS return true to allow the sign-in to complete
+      return true;
     },
-    async jwt({token,account,profile}){
-      if(account && profile){
-        const user = await client.withConfig({useCdn:false}).fetch(AUTHOR_BY_GITHUB_ID,{id:profile?.id})
-        token.id = user?._id;
+
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GITHUB_ID, { id: profile?.id });
+
+        if (user) {
+          token.id = user._id;
+        }
       }
-      return token
+      return token;
     },
-    async session ({session,token}){
-      Object.assign(session,{id:token.id});
-      return session
-    }
+
+    async session({ session, token }) {
+      Object.assign(session, { id: token.id });
+      return session;
+    },
   },
-
-})
+});
